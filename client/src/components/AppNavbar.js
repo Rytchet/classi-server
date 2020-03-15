1e7cb025f7bd9b118cd80ee49ac33f3dabf40510
Oskar <oskarino.g@gmail.com>
2020-03-15 17:35:55 +0000

    Created a react skeleton app

---
+++ client/src/components/AppNavbar.js
@@ -0,0 +1,46 @@
+import React, { Component } from "react";
+
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  Container
+} from "reactstrap";
+
+class AppNavbar extends Component {
+  state = {
+    isOpen: false
+  };
+
+  toggle = () => {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <Navbar color="dark" dark expand="sm" className="mb-5">
+          <Container>
+            <NavbarBrand href="/">Classi</NavbarBrand>
+            <NavbarToggler onClick={this.toggle}></NavbarToggler>
+            <Collapse isOpen={this.state.isOpen} navbar>
+              <Nav className="ml-auto" navbar>
+                <NavItem>
+                  <NavLink href="/">Link one</NavLink>
+                </NavItem>
+              </Nav>
+            </Collapse>
+          </Container>
+        </Navbar>
+      </div>
+    );
+  }
+}
+
+export default AppNavbar;
