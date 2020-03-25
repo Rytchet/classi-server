aa9d853025b32e57cdada4a47cfe6526698dbdd9
Oskar <oskarino.g@gmail.com>
2020-03-25 18:53:32 +0000

    Implemented uploading images for listings

--- routes/api/images.js
+++ routes/api/images.js
@@ -6,6 +6,7 @@ const path = require("path");
 const fs = require("fs");
 
 const User = require("../../models/User");
+const Listing = require("../../models/Listing");
 
 const uploadAvatar = multer({
   dest: "public/images/avatars/temp"
@@ -64,5 +65,64 @@ router.delete("/avatars", auth, (req, res) => {
 //     path.join(__dirname, "../../public/images/avatars", req.params.id + ".jpg")
 //   );
 // });
+
+const uploadListing = multer({
+  dest: "public/images/listings/temp"
+});
+
+router.post(
+  "/listings/:id",
+  auth,
+  uploadListing.array("photos", 10),
+  (req, res) => {
+    Listing.findById(req.params.id).then(listing => {
+      if (req.user.id != listing.user_id) {
+        res.status(403).json({ success: false, msg: "Forbidden" });
+      }
+
+      const baseTargetPath = path.join(
+        __dirname,
+        "../../public/images/listings/",
+        listing.id
+      );
+
+      // Check if all files are the correct format, if not delete all and 403
+      req.files.forEach(file => {
+        const fileExtension = path.extname(file.originalname).toLowerCase();
+        if (fileExtension != ".jpg" && fileExtension != ".png") {
+          req.files.forEach(file => {
+            fs.unlink(file.path, err => {
+              if (err) throw err;
+            });
+          });
+          res.status(403).json({ success: false, msg: "Wrong file types" });
+        }
+      });
+
+      let pathList = [];
+
+      if (!fs.existsSync(baseTargetPath)) {
+        fs.mkdirSync(baseTargetPath);
+      }
+
+      req.files.forEach(file => {
+        let targetPath = baseTargetPath + "\\" + file.originalname;
+        console.log(file.path);
+        console.log(targetPath);
+        fs.rename(file.path, targetPath, err => {
+          console.log(err);
+        });
+        pathList.push(
+          "/images/listings/" + listing.id + "/" + file.originalname
+        );
+      });
+
+      listing.photos = pathList;
+      listing.save();
+
+      res.json({ success: true });
+    });
+  }
+);
 
 module.exports = router;
