0068a446dfbb52bea156b895feb746528d9229df
Oskar <oskarino.g@gmail.com>
2020-04-09 14:28:30 +0100

    Set the token to never expire

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -20,33 +20,26 @@ router.post('/', (req, res) => {
   }
 
   // Check for existing user
-  User.findOne({ email }).then(user => {
+  User.findOne({ email }).then((user) => {
     if (!user) {
       return res.status(400).json({ msg: "User doesn't exist" });
     }
 
     // Validate password
-    bcrypt.compare(password, user.password).then(isMatch => {
+    bcrypt.compare(password, user.password).then((isMatch) => {
       if (!isMatch) return res.status(400).json({ msg: 'Invalid credentials' });
 
-      jwt.sign(
-        { id: user.id },
-        config.get('jwtSecret'),
-        {
-          expiresIn: 3600
-        },
-        (err, token) => {
-          if (err) throw err;
-          res.json({
-            token,
-            user: {
-              id: user.id,
-              name: user.name,
-              email: user.email
-            }
-          });
-        }
-      );
+      jwt.sign({ id: user.id }, config.get('jwtSecret'), (err, token) => {
+        if (err) throw err;
+        res.json({
+          token,
+          user: {
+            id: user.id,
+            name: user.name,
+            email: user.email,
+          },
+        });
+      });
     });
   });
 });
@@ -57,7 +50,7 @@ router.post('/', (req, res) => {
 router.get('/user', auth, (req, res) => {
   User.findById(req.user.id)
     .select('-password')
-    .then(user => res.json(user));
+    .then((user) => res.json(user));
 });
 
 module.exports = router;
